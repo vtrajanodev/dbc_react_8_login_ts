47aa8a1b43aaa37e88f255948e2f20c7397d2d03
Victor Trajano <trajanodev@hotmail.com>
2021-11-29 14:34:30 -0400

    Rotas finalizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { NotFound } from "./components/NotFound";
 import { AuthContextProvider } from "./hooks/useAuth"
 import { EditPessoaContextProvider } from "./hooks/usePessoa";
+import { LinkRoutes } from "./LinkRoutes";
 import { Cadastro } from "./pages/Cadastro";
 import { Login } from "./pages/Login";
 import { Pessoa } from "./pages/Pessoa";
@@ -13,13 +15,7 @@ function App() {
     <BrowserRouter>
       <AuthContextProvider>
         <EditPessoaContextProvider>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Cadastro />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/pessoa" element={<Pessoa />} />
-        </Routes>
-        {/* <Footer /> */}
+          <LinkRoutes />
         </EditPessoaContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
